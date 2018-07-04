0ff09035d0fc1b8ce5d02b79e4bc6100267cdd98
Michael Y. C. Shih <i420cc@gmail.com>
2018-07-04 19:34:37 +0800

    add google ads & embedded barcode scanner

--- app/src/main/java/com/example/mycs/totalkomp/MainActivity.java
+++ app/src/main/java/com/example/mycs/totalkomp/MainActivity.java
@@ -2,6 +2,18 @@ package com.example.mycs.totalkomp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.BarcodeCallback;
+import com.journeyapps.barcodescanner.BarcodeResult;
+import com.journeyapps.barcodescanner.DecoratedBarcodeView;
+import com.journeyapps.barcodescanner.camera.CameraSettings;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +21,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AdView adView = findViewById(R.id.adView);
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/6300978111");
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
+        final TextView scannedResult = findViewById(R.id.barcodeScanned);
+
+        DecoratedBarcodeView decoratedBarcodeView = findViewById(R.id.decoratedBarcodeView);
+        CameraSettings cameraSettings = new CameraSettings();
+        cameraSettings.setRequestedCameraId(0);
+        decoratedBarcodeView.getBarcodeView().setCameraSettings(cameraSettings);
+
+        decoratedBarcodeView.resume();
+
+        decoratedBarcodeView.decodeSingle(new BarcodeCallback() {
+            @Override
+            public void barcodeResult(BarcodeResult result) {
+                scannedResult.setText(result.toString());
+            }
+
+            @Override
+            public void possibleResultPoints(List<ResultPoint> resultPoints) {
+
+            }
+        });
     }
 }
